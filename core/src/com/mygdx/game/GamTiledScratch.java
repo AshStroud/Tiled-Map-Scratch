ba1309ddd5067b653a0d10ad47a1ac91b7fff1f9
Ashleigh <ashcstroud@hotmail.com>
2016-03-29 10:29:19 -0400

    Updated naming conventions

--- core/src/com/mygdx/game/GamTiledScratch.java
+++ core/src/com/mygdx/game/GamTiledScratch.java
@@ -10,7 +10,6 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Rectangle;
@@ -23,104 +22,95 @@ import java.util.ArrayList;
 //                              https://www.youtube.com/watch?v=MT5YwZsQnF8
 
 public class GamTiledScratch extends ApplicationAdapter {
-    private static final int COLS = 4;
-    private static final int ROWS = 4;
+    private static final int nCols = 4;
+    private static final int nRows = 4;
 
     SpriteBatch batch;
-    Texture Sprite;
-    Texture BackGround;
-    TextureRegion[] frames;
+    Texture txSprite;
+    //Texture BackGround;
+    TextureRegion[] trFrames;
     TextureRegion CurrentFrame;
-    float SpriteX = 0;
-    float SpriteY = 0;
-    float SpriteSpeed = 45f;
-    float Time = 0f;
-    Animation animation;
+    float fSpriteX = 0;
+    float fSpriteY = 0;
+    float fSpriteSpeed = 45f;
+    float fTime = 0f;
+    Animation aniMain;
 
     //TiledMap GameMap= new TmxMapLoader().load("IntoTheWoodsRPGMap.tmx");
-    TiledMap GameMap;
-    OrthogonalTiledMapRenderer renderer;
-    OrthographicCamera cam;
-    ArrayList<Rectangle> CollisionDetection;
+    TiledMap tmGameMap;
+    OrthogonalTiledMapRenderer OrhtoTmrRenderer;
+    OrthographicCamera OcCam;
+    ArrayList<Rectangle> arlRectCollisionDetection;
 
 
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        BackGround = new Texture(Gdx.files.internal("lostwoods2.jpg"));
-        Sprite = new Texture(Gdx.files.internal("CinderellaSpriteSheet.png"));
-        TextureRegion[][] tmp = TextureRegion.split(Sprite, Sprite.getWidth() / COLS, Sprite.getHeight() / ROWS);
-        frames = new TextureRegion[COLS * ROWS];
+        //BackGround = new Texture(Gdx.files.internal("lostwoods2.jpg"));
+        txSprite = new Texture(Gdx.files.internal("CinderellaSpriteSheet.png"));
+        TextureRegion[][] tmp = TextureRegion.split(txSprite, txSprite.getWidth() / nCols, txSprite.getHeight() / nRows);
+        trFrames = new TextureRegion[nCols * nRows];
         int index = 0;
-        for (int i = 0; i < ROWS; i++) {
-            for (int j = 0; j < COLS; j++) {
-                frames[index++] = tmp[i][j];
+        for (int i = 0; i < nRows; i++) {
+            for (int j = 0; j < nCols; j++) {
+                trFrames[index++] = tmp[i][j];
             }
         }
-        animation = new Animation(1f, frames);
+        aniMain = new Animation(1f, trFrames);
 
         //Setting Up TiledMap
-        GameMap= new TmxMapLoader().load("IntoTheWoodsRPGMap.tmx");
-        renderer = new OrthogonalTiledMapRenderer(GameMap);
-        CollisionDetection = new ArrayList<Rectangle>();
-
-        /*for(int i =0; i < 20; i++){
-            for(int j=0; j< 20; j++){
-                TiledMapTileLayer current = (TiledMapTileLayer)GameMap.getLayers().get(1);
-                //Cell cell = new TiledMapTileLayer.Cell;
-                TiledMapTileLayer.Cell cell = new TiledMapTileLayer.Cell();
-            }
-        }*/
-
+        tmGameMap= new TmxMapLoader().load("IntoTheWoodsRPGMap.tmx");
+        OrhtoTmrRenderer = new OrthogonalTiledMapRenderer(tmGameMap);
+        arlRectCollisionDetection = new ArrayList<Rectangle>();
 
         //Setting Up Orthographic Camera
-        cam = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        OcCam = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
 
     @Override
     public void render() {
         //Rendering Sprite
-        if (Time < 4) {
-            Time += Gdx.graphics.getDeltaTime();
+        if (fTime < 4) {
+            fTime += Gdx.graphics.getDeltaTime();
         } else {
-            Time = 0;
+            fTime = 0;
         }
 
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        CurrentFrame = animation.getKeyFrame(0);
+        CurrentFrame = aniMain.getKeyFrame(0);
 
         if (Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT)) {
-            SpriteX -= Gdx.graphics.getDeltaTime() * SpriteSpeed;
-            CurrentFrame = animation.getKeyFrame(4 + Time);
+            fSpriteX -= Gdx.graphics.getDeltaTime() * fSpriteSpeed;
+            CurrentFrame = aniMain.getKeyFrame(4 + fTime);
         }
         if (Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)) {
-            SpriteX += Gdx.graphics.getDeltaTime() * SpriteSpeed;
-            CurrentFrame = animation.getKeyFrame(8 + Time);
+            fSpriteX += Gdx.graphics.getDeltaTime() * fSpriteSpeed;
+            CurrentFrame = aniMain.getKeyFrame(8 + fTime);
         }
         if (Gdx.input.isKeyPressed(Input.Keys.DPAD_UP)) {
-            SpriteY += Gdx.graphics.getDeltaTime() * SpriteSpeed;
-            CurrentFrame = animation.getKeyFrame(12 + Time);
+            fSpriteY += Gdx.graphics.getDeltaTime() * fSpriteSpeed;
+            CurrentFrame = aniMain.getKeyFrame(12 + fTime);
         }
         if (Gdx.input.isKeyPressed(Input.Keys.DPAD_DOWN)) {
-            SpriteY -= Gdx.graphics.getDeltaTime() * SpriteSpeed;
-            CurrentFrame = animation.getKeyFrame(0 + Time);
+            fSpriteY -= Gdx.graphics.getDeltaTime() * fSpriteSpeed;
+            CurrentFrame = aniMain.getKeyFrame(0 + fTime);
         }
 
         //Rendering Tiled Map
-        renderer.setView(cam);
-        renderer.render();
+        OrhtoTmrRenderer.setView(OcCam);
+        OrhtoTmrRenderer.render();
 
         //OrthoGraphic Camera
-        cam.position.set(SpriteX, SpriteY, 0);
-        batch.setProjectionMatrix(cam.combined);
-        cam.update();
+        OcCam.position.set(fSpriteX, fSpriteY, 0);
+        batch.setProjectionMatrix(OcCam.combined);
+        OcCam.update();
 
         //Draw Sprites
         batch.begin();
         //batch.draw(BackGround, 0, 0);
-        batch.draw(CurrentFrame, (int) SpriteX, (int) SpriteY);
+        batch.draw(CurrentFrame, (int) fSpriteX, (int) fSpriteY);
         batch.end();
     }
 
